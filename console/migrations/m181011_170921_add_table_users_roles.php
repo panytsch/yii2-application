d7317b2cd22007689cc737a364c89287d22c9b16
panytsch <gfyroman@gmail.com>
2018-10-11 20:23:14 +0300

    roles

---
+++ console/migrations/m181011_170921_add_table_users_roles.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m181011_170921_add_table_users_roles
+ */
+class m181011_170921_add_table_users_roles extends Migration
+{
+    private $table = 'users_roles';
+    private $idx = 'idx-user-roles';
+    private $fk = 'idx-user-roles';
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable($this->table, [
+            'id' => $this->primaryKey(),
+            'role_name' => $this->string(50)->notNull()
+        ]);
+        $this->addColumn('user', 'role_id', $this->integer(11)->notNull()->after('id'));
+        $this->createIndex($this->idx, 'user', 'role_id');
+        $this->addForeignKey(
+            $this->fk,
+            'user',
+            'role_id',
+            $this->table,
+            'id'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey($this->fk, 'user');
+        $this->dropIndex($this->idx, 'user');
+        $this->dropColumn('user', 'role_id');
+        $this->dropTable($this->table);
+    }
+}
